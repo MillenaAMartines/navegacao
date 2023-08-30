099cf51b86ef3e45d3a320c56b9cb375b7b368f7
Millena Araújo Martines <millenamartinez@Henriques-MacBook-Air.local>
2023-08-30 14:58:42 +0700

    first commit

---
+++ src/views/App.jsx
@@ -0,0 +1,14 @@
+
+import Content from '../componets/layout/Content'
+import Menu from '../componets/layout/Menu'
+import './App.css'
+import React from 'react'
+
+const App = props => (
+    <div className="App">
+        <Menu />
+        <Content />
+    </div>
+)
+
+export default App 
